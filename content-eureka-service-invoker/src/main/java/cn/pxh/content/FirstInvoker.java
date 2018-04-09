1c3f3fd487fd463d9542672973dca22fd2f6b4c0
pppxh <903404312@qq.com>
2018-04-09 17:15:01 +0800

    test eureka

---
+++ content-eureka-service-invoker/src/main/java/cn/pxh/content/FirstInvoker.java
@@ -0,0 +1,14 @@
+package cn.pxh.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FirstInvoker {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstInvoker.class, args);
+    }
+
+}
---
+++ content-eureka-service-provider/src/main/java/cn/pxh/content/FirstServiceProvider.java
@@ -0,0 +1,13 @@
+package cn.pxh.content;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FirstServiceProvider {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(FirstServiceProvider.class).run(args);
+    }
+}
